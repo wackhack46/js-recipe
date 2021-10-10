f7ddaa7c2c1e9127be9d6a829ae7b59c0dc649f4
wackhack46 <kazato1020@gmail.com>
2021-10-10 17:18:20 +0900

    [DogAPIで非同期処理],ワンちゃん切り替えボタンを作りました

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -1,7 +1,28 @@
-const question = "ゲーム市場、最も売れたゲーム機は次のうちどれ　"；
-const answer =["スーパーファミコン",
-"PS2",
-"任天堂DS"
-];
+const choice1 = document.getElementById("choice-1")
+const choice2 = document.getElementById("choice-2")
+const choice3 = document.getElementById("choice-3")
+const feedback = document.getElementById("feedback")
 
-const corect = "任天堂DS" ;
+const feedbacks = [
+  "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+  "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+  "正解！ガニメデは、木星の第三惑星だよ！",
+]
+
+// 正解を判定する
+const choose = function(choiceNumber) {
+  feedback.textContent = feedbacks[choiceNumber]
+}
+
+choice1.onclick = function() {
+  // 0 番目の選択肢を選択
+  choose(0)
+}
+choice2.onclick = function() {
+  // 1 番目の選択肢を選択
+  choose(1)
+}
+choice3.onclick = function() {
+  // 2 番目の選択肢を選択
+  choose(2)
+}
--- 04_ゼミ/week0/gitで保存する.md
+++ 04_ゼミ/week0/gitで保存する.md
@@ -5,13 +5,3 @@ git add .
 git commit -m "コミット"
 git push origin master
 ```
-
-const genkiFunction = function(number) {
-for (let n = 1; n <= number; n++) {
-if (n % 3 === 0 || n % 10 === 3 || (30 <= n && n < 40)) {
-console.log(`${n}!!!!!!`)
-} else {
-console.log(n)
-}
-}
-}
