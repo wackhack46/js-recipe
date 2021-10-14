b2b188bdd303911e4ccaf785be3fff3325ca7e1f
wackhack46 <kazato1020@gmail.com>
2021-10-15 01:06:57 +0900

    [memoアプリ関数化]まで学習しました

--- 04_ゼミ/week2/pairwork/main.js
+++ 04_ゼミ/week2/pairwork/main.js
@@ -3,16 +3,22 @@ const memoContainer = document.getElementById("memo-container")
 const addButton = document.getElementById("add-button")
 
 addButton.onclick = function() {
-  console.log(memoInput.value)
-  // タグ名を指定して要素をつくる
   const memo = document.createElement("div")
   const deleteButton = document.createElement("button")
+  tsuika(memo, deleteButton)
+  sakujo(memo, deleteButton)
+}
+
+const sakujo = function(memo, deleteButton) {
   deleteButton.textContent = "削除"
-  memo.textContent = memoInput.value // <div class="card>こんばんは</div>
-  memoContainer.append(memo)
-  memo.append(deleteButton)
-  memoInput.value = ""
   deleteButton.onclick = function() {
     memo.remove()
   }
+}
+
+const tsuika = function(memo, deleteButton) {
+  memo.textContent = memoInput.value
+  memoContainer.append(memo)
+  memo.append(deleteButton)
+  memoInput.value = ""
 }
